506badd07c0f7b3ff5b4c7f622e99439023309f8
creativity2472001-ctrl <creativity2472001@gmail.com>
2026-02-25 04:52:20 +0200

    Delete Config.py

--- Config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
-    DATABASE_PATH = "users.db"
-    PORT = int(os.getenv("PORT", 8080))
